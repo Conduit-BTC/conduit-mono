37f03192c9ec59b7a73ad38ac9b45af18124cae9
Dylan Golow <dylangolow@gmail.com>
2026-03-25 02:07:36 +0000

    Fix app startup and React resolution

--- apps/merchant/src/main.tsx
+++ apps/merchant/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { AuthProvider, connectNdk, ensureCommerceCacheScope } from "@conduit/core"
+import { AuthProvider, connectNdk } from "@conduit/core"
 import { routeTree } from "./routeTree.gen"
 import "./styles/index.css"
 
@@ -17,7 +17,6 @@ declare module "@tanstack/react-router" {
   }
 }
 
-void ensureCommerceCacheScope()
 connectNdk()
 
 createRoot(document.getElementById("root")!).render(
